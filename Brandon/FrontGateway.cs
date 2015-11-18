cf1ca75fce1852421f568d8c26d2d5c33bce0d2d
Park Hyunjeong <anyflow@gmail.com>
2015-11-18 23:41:18 +0900

    initial commit.

---
+++ Brandon/FrontGateway.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Brandon.Gateway
+{
+    class FrontGateway
+    {
+        private String ServerAddress { get; set; }
+        
+        public async void createSession()
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, ServerAddress);
+                request.Content = new StringContent("", Encoding.UTF8, "application/json");
+
+                var response = await client.SendAsync(request);
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+            }
+
+                
+        }
+    }
+}
