ed11bd9638162aa34bf3f00b2b7d32504956cf26
Hyunjeong park <anyflow@gmail.com>
2015-11-21 17:47:34 +0900

    Test skeleton and sample added.

---
+++ Entrypoint/FrontGateway.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Brandon.Model;
+using Newtonsoft.Json;
+
+namespace Brandon.Gateway
+{
+    public class FrontGateway
+    {
+        private String ServerAddress { get; set; }
+        
+        public async void CreateSession()
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, ServerAddress);
+                request.Content = new StringContent("", Encoding.UTF8, "application/json");
+
+                var response = await client.SendAsync(request);
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
---
+++ Entrypoint/Model/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brandon.Model
+{
+    class Room
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string SecretKey { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
