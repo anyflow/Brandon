dac263709f5d7b6aa24d7c4786f23f6ee8b97f08
Park Hyunjeong <anyflow@gmail.com>
2015-11-21 17:56:21 +0900

    .gitignore updated.

---
+++ Entrypoint/Test/UtilityTest.cs
@@ -0,0 +1,59 @@
+﻿using Brandon.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brandon.Test
+{
+    [TestClass]
+    public class UtilityTest
+    {
+        private string generateJson()
+        {
+            List<User> users = new List<User>();
+            var user1 = new User()
+            {
+                Id = "id1",
+                Name = "name1",
+                Description = "description1",
+                CreateDate = DateTime.Now
+            };
+            users.Add(user1);
+
+            var user2 = new User()
+            {
+                Id = "id2",
+                Name = "name2",
+                Description = "description2",
+                CreateDate = DateTime.Now
+            };
+            users.Add(user2);
+
+            return JsonConvert.SerializeObject(users);
+        }
+
+        [TestMethod]
+        public void serializationTest()
+        {
+            string json = generateJson();
+
+            System.Diagnostics.Debug.Write(json);
+
+            StringAssert.Contains(json, "name2");
+        }
+
+        [TestMethod]
+        public void DeserializationTest()
+        {
+            string json = generateJson();
+
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(json);
+
+            Assert.AreEqual<String>("name2", users[1].Name);
+        }
+    }
+}
